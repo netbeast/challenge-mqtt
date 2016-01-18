207baa8ebaa3c003c55f7b9a6a72e4eec65d1528
Lourdes Liro <lirosalinas@gmail.com>
2016-01-18 17:29:15 +0100

    correccion de test.js

--- test.js
+++ test.js
@@ -1,13 +1,3 @@
-var http = require('http')
-var path = require('path')
-
-var argv = require('minimist')(process.argv.slice(2))
-var express = require('express')
-var mosca = require('mosca')
-
-var app = express()
-var mqttServ = new mosca.Server({})
-
 var mqtt = require('mqtt')
 
 
@@ -24,22 +14,3 @@ setInterval(function () {
 	    cliente.publish("mqtt/demo", "notificacion periodica mandada!")
 	    }, 1000)
 
-
-
-
-
-app.use(express.static('public'))
-app.use('/mosca', express.static(path.dirname(require.resolve('mosca')) + '/public'))
-
-
-
-var httpServ = http.createServer(app)
-
-
-mqttServ.attachHttpServer(httpServ)
-
-httpServ.listen(argv.port || 8000, function () {
-  console.log('Example app listening at http://%s:%s',
-	      httpServ.address().address,
-	      httpServ.address().port)
-		})
